2ccf68f2e2da138d3e928e636968117ac0b28850
James <jamesncoupland@gmail.com>
2021-11-15 14:56:01 +0000

    html css layout start

---
+++ script.js
@@ -0,0 +1,35 @@
+
+let a = 20;
+let b = 5;
+
+
+//addition function
+function add() {
+   return a + b;   
+}
+add();
+console.log(add())
+
+
+//subtraction function
+function sub() {
+    return a - b;    
+ } 
+ sub(); 
+ console.log(sub())
+
+
+ //multiply function
+function multi() {
+    return a * b;    
+ } 
+ multi(); 
+ console.log(multi())
+
+
+//divide function
+function divide() {
+    return a / b;    
+ } 
+ divide(); 
+ console.log(divide())
